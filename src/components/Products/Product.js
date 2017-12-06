269eba58e2f64e1b853cd0dc88bc074c29e0c537
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-05 22:29:39 -0300

    Fixed function to convert hex values

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -46,7 +46,7 @@ const Product = (props) => [
                         </div>
                         <div className="product-data">
                             <div className="product-data-block">
-                                <span className="product-name">{hexToString(result.name)}</span>
+                                <span className="product-name">{parseHex(result.name)}</span>
                             </div>
                             <div className="product-data-block">
                                 <span className="product-store">{result.store}</span>
@@ -63,75 +63,74 @@ const Product = (props) => [
     </div>
 ]
 
+// Manually replace hex values. Though hexToString correctly parses hex values,
+// for some reason they don't get displayed in the Browser
+function parseHex (str) {
+    return str
+    .replace(/&#x2013;/gi, ' ')
+    .replace(/&#xa0;/gi, ' ')
+    .replace(/&#xc4;/gi, 'Á')
+    .replace(/&#xe1;/gi, 'á')
+    .replace(/&#xc9;/gi, 'É')
+    .replace(/&#xe9;/gi, 'é')
+    .replace(/&#xcd;/gi, 'Í')
+    .replace(/&#xed;/gi, 'í')
+    .replace(/&#xd3;/gi, 'Ó')
+    .replace(/&#xF3;/gi, 'ó')
+    .replace(/&#xda;/gi, 'Ú')
+    .replace(/&#xfa;/gi, 'ú')
+    .replace(/&#xdc;/gi, 'Ü')
+    .replace(/&#xfc;/gi, 'ü')
+    .replace(/&#xd1;/gi, 'Ñ')
+    .replace(/&#xf1;/gi, 'ñ')
+}
+
 // Transforms a string that contains hex NCR code to a normal string
-function hexToString (inputStr) {
-    // Look for &# and separate the string in two. Then look for a ";"" and separate the string in two again.
-    // Repeat until you have every expression that starts with &# and ends with ";""
-    // For every expression captured replace &# with a 0 and erase the ";"
-    // Transform the resulting expressions using fromCharCode and concatenate the resulting string
-    // console.log('========= InputStr')
-    // console.log(inputStr)
-    inputStr = inputStr.replace('&#x2013;', '-')
-    let startStr = '&#'
-    let endStr = ';'
-    let start = inputStr.indexOf(startStr)
-    let end = inputStr.indexOf(endStr)
-    if (start > 0) {
-        let firstPart = inputStr.slice(0, start) 
-        let secondPart = inputStr.slice(start, end + 1)
-        secondPart = secondPart.replace('&#', '0').replace(';', '')
-        let thirdPart = inputStr.slice(end + 1)
-        // console.log('===== First part is: ')
-        console.log(firstPart)
-        // console.log('===== SEcond part is: ')
-        console.log(secondPart)
-        console.log(hex2b(secondPart).length)
-        // console.log('===== Third part is: ')
-        console.log(thirdPart)
-        let newString = firstPart + hex2b(secondPart).slice(1) + thirdPart
-        // console.log('===== Result is')
-        console.log(newString)
-        hexToString(newString)
-        // let rucursive = newString.indexOf(startStr)
-        // if (rucursive > 0) { 
-        //     hexToString(newString)
-        // } else {
-        //     console.log('check 1')
-        //     return newString
-        // }
-    } else {
-        // console.log('check 2')
-        return inputStr
-    }
-}
-
-// function unicodeToChar(text) {
-//     return text.replace(/\\u[\dA-F]{4}/gi, 
-//         function (match) {
-//             return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
-//         });
-// }
-// function a2hex(str) {
-//     var arr = [];
-//     for (var i = 0, l = str.length; i < l; i ++) {
-//       var hex = Number(str.charCodeAt(i)).toString(16);
-//       arr.push(hex);
+// function hexToString (inputStr) {
+//     // Look for &# and separate the string in two. Then look for a ";"" and separate the string in two again.
+//     // Repeat until you have every expression that starts with &# and ends with ";""
+//     // For every expression captured replace &# with a 0 and erase the ";"
+//     // Transform the resulting expressions using fromCharCode and concatenate the resulting string
+//     // console.log('========= InputStr')
+//     // console.log(inputStr)
+//     inputStr = inputStr.replace('&#x2013;', '-')
+//     let startStr = '&#'
+//     let endStr = ';'
+//     let start = inputStr.indexOf(startStr)
+//     let end = inputStr.indexOf(endStr)
+//     if (start > 0) {
+//         let firstPart = inputStr.slice(0, start) 
+//         let secondPart = inputStr.slice(start, end + 1)
+//         secondPart = secondPart.replace('&#', '0').replace(';', '')
+//         let thirdPart = inputStr.slice(end + 1)
+//         // console.log('===== First part is: ')
+//         console.log(firstPart)
+//         // console.log('===== SEcond part is: ')
+//         console.log(secondPart)
+//         console.log(hex2b(secondPart).length)
+//         // console.log('===== Third part is: ')
+//         console.log(thirdPart)
+//         let newString = firstPart + hex2b(secondPart).slice(1) + thirdPart
+//         // console.log('===== Result is')
+//         console.log(newString)
+//         hexToString(newString)
+//         // let rucursive = newString.indexOf(startStr)
+//         // if (rucursive > 0) { 
+//         //     hexToString(newString)
+//         // } else {
+//         //     console.log('check 1')
+//         //     return newString
+//         // }
+//     } else {
+//         // console.log('check 2')
+//         return inputStr
 //     }
-//     return arr.join('');
-//   }
-  
-//   function hex2a(hexx) {
-//       var hex = hexx.toString();//force conversion
-//       var str = '';
-//       for (var i = 0; i < hex.length; i += 2)
-//           str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-//       return str;
-//   }
+// }
 
-  function hex2b(hex) {
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
+// function hex2b(hex) {
+//     var str = '';
+//     for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+//     return str;
+// }
 
 export default Product;
