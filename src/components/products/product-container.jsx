c86b58dc95ee3964923dbcfdff1cc77e6fb5977f
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-24 17:31:39 -0300

    Heroku push fix

--- src/components/products/product-container.jsx
+++ src/components/products/product-container.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import ProductGrid from './product-grid'
+// import React, { Component } from 'react'
+// import ProductGrid from './product-grid'
 
-class ProductContainer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            results = []
-        }
-    }
+// class ProductContainer extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             results = []
+//         }
+//     }
 
-    render() {
-        const results = this.state.results
+//     render() {
+//         const results = this.state.results
 
-        return (
-            <ProductGrid 
-                results= {this.state.results}
-            />
-        )
-    }
-}
+//         return (
+//             <ProductGrid 
+//                 results= {this.state.results}
+//             />
+//         )
+//     }
+// }
