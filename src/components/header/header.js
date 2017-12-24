7dd9112c90b364d6d8ce41bf3f9da30a66704a8a
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-24 18:06:18 -0300

    Added new files /header

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css'
+
+const Header = (props) => (
+    <div className="logo">
+        <span>Bazar de Mascotas</span>
+    </div>  
+);
+
+export default Header
