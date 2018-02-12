df3fbffffa5cb696f7f168c98b96ef7509cd9b27
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2018-02-12 12:57:09 -0300

    Product-container now fetch items on initial mount

--- src/components/products/product-container.jsx
+++ src/components/products/product-container.jsx
@@ -23,7 +23,35 @@ class SearchContainer extends Component {
 
     componentDidMount() {
         const { location } = this.props
-        return this.props.getActiveRoute(location.pathname)
+        this.props.getActiveRoute(location.pathname)
+
+        return Client.search('royal canin maxi', results => {
+            const storeFilters = results
+            // retrieve the store values from the results
+            .map(result => result.store)
+            // get an array with only the resulting stores
+            .filter((store, index, self) => self.indexOf(store) === index)
+            // Create the filter objects defaulting to true to show all the results at first
+            .map(storeName => ({id: storeName, checked: true}))
+        
+            const petFilters = results
+                .map(result => result.animal)
+                .filter((pet, index, self) => self.indexOf(pet) === index)
+                .map(petKind => ({id: petKind, checked: true}))
+            
+            const categoryFilters = results
+                .map(result => result.category)
+                .filter((category, index, self) => self.indexOf(category) === index)
+                .map(category => ({id: category, checked: true}))
+
+            return this.setState({
+                isLoading: false,
+                results: results,
+                storeFilters: storeFilters,
+                petFilters: petFilters,
+                categoryFilters: categoryFilters        
+            })
+        })
     }
 
     handleInputChange = e => this.setState({ searchValue: e.target.value })
