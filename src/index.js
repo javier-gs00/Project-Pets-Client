ed006041c91cf0d306341c17cab66fa1cb91a0de
Javier Gutierrez <javiergs1106@gmail.com>
2019-02-24 21:22:07 -0300

    Move the font awesome library into its own file

--- src/index.js
+++ src/index.js
@@ -2,63 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import projectPetsApp from './reducers/reducers'
-
 import App from './components/App'
+import './font-awsesome-icons'
 import './assets/style/style.css'
 // import './style/style.less'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faAngleDown,
-  faAmbulance,
-  faBars,
-  faBuilding,
-  faCar,
-  faClock,
-  faEnvelope,
-  faFutbol,
-  faGlobe,
-  faMapMarker,
-  faMedkit,
-  faPaw,
-  faPhone,
-  faSearch,
-  faSignInAlt,
-  faSpinner,
-  faShower,
-  faSortNumericDown,
-  faSortNumericUp,
-  faThLarge,
-  faUndoAlt,
-  faUserMd,
-  faUtensils
-} from '@fortawesome/free-solid-svg-icons'
-
-library.add(
-  faAngleDown,
-  faAmbulance,
-  faBars,
-  faBuilding,
-  faCar,
-  faClock,
-  faEnvelope,
-  faFutbol,
-  faGlobe,
-  faMapMarker,
-  faMedkit,
-  faPaw,
-  faPhone,
-  faSearch,
-  faSignInAlt,
-  faSpinner,
-  faShower,
-  faSortNumericDown,
-  faSortNumericUp,
-  faThLarge,
-  faUndoAlt,
-  faUserMd,
-  faUtensils
-)
 
 let store = createStore(projectPetsApp)
 
